8dabede8a88bbb3a9f18f5781c7c2d48f77c9d1d
semnavy70 <70761590+semnavy70@users.noreply.github.com>
2022-06-19 05:52:57 +0700

    add namespace Uri and ContentResolver

--- android/src/main/java/com/softmaestri/notification/channel/flutter_notification_channel/FlutterNotificationChannelPlugin.java
+++ android/src/main/java/com/softmaestri/notification/channel/flutter_notification_channel/FlutterNotificationChannelPlugin.java
@@ -8,6 +8,8 @@ import android.media.AudioAttributes;
 import android.os.Build;
 import android.provider.Settings;
 import android.util.Log;
+import android.net.Uri;
+import android.content.ContentResolver;
 
 import androidx.annotation.NonNull;
 
